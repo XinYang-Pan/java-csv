1d875d53f394da660f6083af09ed21fa5710f558
SeanPAN <seanpan2008@gmail.com>
2016-01-17 10:27:10 +0800

    test commit msg

--- src/test/java/com/blueo/common/test/Tester.java
+++ src/test/java/com/blueo/common/test/Tester.java
@@ -6,6 +6,7 @@ import com.blueo.csv.test.Person;
 public class Tester {
 
 	public static void main(String[] args) {
+		CodeGenerator.generateSetting(Person.class);
 		CodeGenerator.generateSetting(Person.class, Person.class);
 		CodeGenerator.generateSetting(Person.class, Person.class, "p1", "p2");
 	}
